327e43563805c3efc8372bb6e99c44d3600cf41f
emilelemoine <lemoine.emile@gmail.com>
2021-02-19 23:30:57 -0500

    Add time delay

--- src/entropy/sample_entropy.jl
+++ src/entropy/sample_entropy.jl
@@ -12,29 +12,29 @@ julia> sample_entropy([1, 2, 3, 1, 2, 3], m=2, r=0.2, τ=1)
 0.6931471805599453
 ```
 """
-function sample_entropy(x::AbstractArray, m, r)
+function sample_entropy(x::AbstractArray, m=2, r=0.2, τ=1)
     #=
     x -> normalized signal (1d vector)
     m -> embedding dimension (must be > length of signal)
     r -> tolerance factor
-    τ -> timestep =#
-    # x = x[1:τ:end]
-    # TODO
-    # resample with moving average (length of samples = τ)
+    τ -> delay =#
+    # TODO For multiscale entropy: wavelet to isolate bands
     N = length(x)
     σ = std(x)
     tolerance = σ * r
 
     # Create template vectors (of length m + 1)
     matches = zeros(m + 1, N)
-    fill!(matches, x[end])
-    for i in 1:m + 1
+    for i in 1:m
         matches[i, 1:(N + 1 - i)] = x[i:end]
     end
 
+    matches[m + 1, 1:(N + 1 - m - τ)] = x[m + τ:end]
+    matches = matches[:, 1:N + 1 - m - τ]
+
     # Calculate pairwise distances for templates of length m
-    dist_m = zeros(N, N)
-    dist_m = pairwise!(dist_m, Chebyshev(), matches[1:m,:], dims=2)
+    dist_m = zeros(N - m, N - m)
+    pairwise!(dist_m, Chebyshev(), matches[1:m,:], dims=2)
     # Extract upper triangle of distance matrix
     dist_m = dist_m[tril!(trues(size(dist_m)), -1)]
     # Count pairs of template that are within tolerance
@@ -45,11 +45,12 @@ function sample_entropy(x::AbstractArray, m, r)
     end
 
     # Repeat for templates m+1
-    dist_m1 = zeros(N, N)
-    dist_m1 = pairwise!(dist_m1, Chebyshev(), matches[1:m + 1,:], dims=2)
+    dist_m1 = zeros(N - m, N - m)
+    pairwise!(dist_m1, Chebyshev(), matches[1:m + 1,:], dims=2)
     dist_m1 = dist_m1[tril!(trues(size(dist_m1)), -1)]
     A = count(x -> x <= tolerance, dist_m1)
 
-    sampen = -log(A / B)
+    -log(A / B)
+    # Return object with B, A and CI around sampen
 
 end
--- test/test_sample_entropy.jl
+++ test/test_sample_entropy.jl
@@ -1,14 +1,23 @@
 using epilepsy: sample_entropy
 using NPZ: npzread
 
-all_eeg = npzread("test_data/eeg_processed.npz")
-eeg = all_eeg["arr_1"]
-eeg_segment = eeg[1, 2000:10000]
+eeg = npzread("test_data/eeg_processed.npy")
+eeg_segment = eeg[1, 1:10000]
 
 sine = sin.(collect(1:0.1:1000))
 
-m = 2
+m = 3
 r = 0.2
 
 @test sample_entropy(ones(10), m, r) == 0.0
 @test sample_entropy(sine, m, r) <= 0.5
+
+
+
+for i in 0:10
+    for length in [200, 2000, 20000]
+        x = eeg[1, 1:length]
+        println(sample_entropy(x, i, r))
+        plot!(x)
+    end
+end
